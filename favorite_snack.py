5cee9d27ba9100f71a7c9b5e7b866f310e032393
tonyeli <tonyeli.pa@gmail.com>
2020-05-21 16:04:07 -0400

    favorite_snack.py

---
+++ favorite_snack.py
@@ -0,0 +1,33 @@
+ Favorite Snack
+
+ a = my favorite Snack
+ b = Melanie's favorite snack
+ c = brownies
+ d = plantain chips
+ a = brownies
+ b = plantain chips
+
+ What to print for Tonyeli
+favorite snack = brownies
+ print a * 100
+ 'browiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowiniesbrowinies'
+
+Melanie's favorite snack
+What to print for Melanie
+favorite snack = plantain chips
+print b * 100
+'plantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chipsplantain chips'
+
+5 Essential groceries
+
+print (1,2,,3,4,5) = (fish, tomatoes, garlic, peppe, onion)
+1. Fish 
+2. Tomatoes
+3. Garlic
+4. Pepper
+5. Onion
+
+print each item seperately in order of item as indicated above
+
+
+
